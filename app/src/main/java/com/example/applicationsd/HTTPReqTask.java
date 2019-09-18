81a8eca0e6f7ba185074e4b0ba1d853cd7300c83
Jonathas Eloi <jonathaseloi@gmail.com>
2019-09-17 22:25:56 -0300

    .

---
+++ app/src/main/java/com/example/applicationsd/HTTPReqTask.java
@@ -0,0 +1,64 @@
+package com.example.applicationsd;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class HTTPReqTask extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+                HttpURLConnection urlConnection = null;
+
+                try {
+                        JsonObject postData = new JsonObject();
+                        postData.addProperty("nome", params[0]);
+                        postData.addProperty("sobrenome", params[1]);
+                        postData.addProperty("email", params[2]);
+
+                        URL url = new URL("http://192.168.0.10/server.php");
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.setRequestProperty("Content-Type", "application/json");
+                        urlConnection.setRequestMethod("POST");
+                        urlConnection.setDoOutput(true);
+                        urlConnection.setDoInput(true);
+                        urlConnection.setChunkedStreamingMode(0);
+
+                        OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                        writer.write(postData.toString());
+                        writer.flush();
+
+                        int code = urlConnection.getResponseCode();
+                        if (code !=  201 && code != 200) {
+                                throw new IOException("Invalid response from server: " + code);
+                        }
+
+                        BufferedReader rd = new BufferedReader(new InputStreamReader(
+                        urlConnection.getInputStream()));
+
+                        String line;
+                        while ((line = rd.readLine()) != null) {
+                                Log.i("data", line);
+                        }
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+                        if (urlConnection != null) {
+                        urlConnection.disconnect();
+                        }
+                }
+
+                return null;
+        }
+}
